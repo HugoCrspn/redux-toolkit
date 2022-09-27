0e0a9f5e5d729a50647dfdee29e7932fb10d7dfa
HugoCrspn <hugocrespinpro@gmail.com>
2022-09-27 13:05:58 +0200

    CSS for card is done #12

--- src/components/FigurineItem.js
+++ src/components/FigurineItem.js
@@ -1,25 +1,36 @@
 import React from 'react';
+import { useState } from 'react';
 
 const FigurineItem = (props) => {
 
     const { figurine } = props;
+
+    const [imgFull, setImgFull] = useState(false);
+    const imgToggle = () => {
+        setImgFull(!imgFull);
+    }
+
     
     return(
         <div className="card">
             <div className="top-card">
                 <h2>{figurine.name}</h2>
-                <p className='category'>{figurine.category}<span>#{figurine.number}</span></p>
+                <p className='category'>{figurine.category}<span> #{figurine.number}</span></p>
                 <p className="description">{figurine.description}</p>
             </div>
             <div className="bottom-card">
                 <div className="left">
                     <p className="date">{figurine.purchaseDate}</p>
-                    <p className="checkbox-style"><span></span>With Box</p>
-                    <p className="checkbox-style"><span></span>Chase</p>
-                    <p className="checkbox-style"><span></span>Exclusive</p>
+                    <div className='informations'>
+                        <p className='checkbox-style'><span className={figurine.box ? "active" : ""}></span>With Box</p>
+                        <p className='checkbox-style'><span className={figurine.chase ? "active" : ""}></span>Chase</p>
+                        <p className='checkbox-style'><span className={figurine.exclusive ? "active" : ""}></span>Exclusive</p>
+                    </div>
                 </div>
-                <img src={figurine.imgUrl} alt={`Représente la figurine : ${figurine.name}`} />
+                <img id="card-img" className={imgFull ? "imgFullBackground" : ""} onClick={imgToggle} src={figurine.imgUrl} alt={`Représente la figurine : ${figurine.name}`} />
             </div>
+            {/* <div className='settings-icons'></div> */}
+            <img className='settings-icons' src="./img/icons/settings.svg" alt="" />
         </div>
     )
 }
--- src/store.js
+++ src/store.js
@@ -3,7 +3,7 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 const figurineSlice = createSlice({
     name: "figurine",
     initialState: [
-        {id: 1, number: 503, name: "Bartman", category: "Pop Television", description: "Offert pas Jacques lors de mon anniversaire, état neuf", purchaseDate: "20/07/2022", box: false, chase: false, exclusive: true, imgUrl: "./img/bartman.jpg"},
+        {id: 1, number: 503, name: "Bartman", category: "Pop Television", description: "Offert pas Jacques lors de mon anniversaire, état neuf", purchaseDate: "20/07/2022", box: true, chase: false, exclusive: true, imgUrl: "./img/bartman.jpg"},
         {id: 2, number: 421, name: "Eleven with eggos", category: "Pop Television", description: "Chase édition acheté à Geek'n'pop dans un magasin à Tours, état neuf", purchaseDate: "14/05/2022", box: true, chase: true, exclusive: true, imgUrl: "./img/eleven.jpg"},
         {id: 3, number: 121, name: "Matt Murdock", category: "Pop Marvel", description: "Récupéré d'occasion dans un micromania, pas très cher mais sans boite", purchaseDate: "05/02/2022", box: false, chase: false, exclusive: false, imgUrl: "./img/mattmurdock.jpg"},
         {id: 4, number: 243, name: "Chell", category: "Pop Games", description: "Issue de la licence Portal, figurine accompagnée du cube iconique du jeux, très bon état, avec boite", purchaseDate: "28/11/2022", box: true, chase: false, exclusive: false, imgUrl: "./img/shell.jpg"}

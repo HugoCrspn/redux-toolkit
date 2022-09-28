bff537b1a214f1b0cb815f7b819cb24f0ed1b06c
HugoCrspn <hugocrespinpro@gmail.com>
2022-09-28 14:11:33 +0200

    Sort by publishDate + refactoring in form.js #18

--- src/components/FigurinesList.js
+++ src/components/FigurinesList.js
@@ -8,9 +8,18 @@ const FigurinesList = () => {
 
     const figurines = useSelector(state => state.figurine);
 
+    const figurinesSortByDate = figurines
+    .slice()
+    .sort((a, b) => {
+        if (b.publishDate < a.publishDate) {return -1;}
+        if (b.publishDate > a.publishDate) {return 1;}
+        return 0;
+    });
+
     return(
         <div className='container figurines-container'>
-            {figurines.map((figurine) => (
+            {figurinesSortByDate
+            .map((figurine) => (
                 <FigurineItem 
                     key={figurine.id}
                     figurine={figurine}
--- src/store.js
+++ src/store.js
@@ -3,10 +3,10 @@ import { configureStore, createSlice } from "@reduxjs/toolkit";
 const figurineSlice = createSlice({
     name: "figurine",
     initialState: [
-        {id: 1, number: 503, name: "Bartman", category: "Pop Television", description: "Offert pas Jacques lors de mon anniversaire, état neuf", purchaseDate: "20/07/2022", box: true, chase: false, exclusive: true, imgUrl: "./img/bartman.jpg"},
-        {id: 2, number: 421, name: "Eleven with eggos", category: "Pop Television", description: "Chase édition acheté à Geek'n'pop dans un magasin à Tours, état neuf", purchaseDate: "14/05/2022", box: true, chase: true, exclusive: true, imgUrl: "./img/eleven.jpg"},
-        {id: 3, number: 121, name: "Matt Murdock", category: "Pop Marvel", description: "Récupéré d'occasion dans un micromania, pas très cher mais sans boite", purchaseDate: "05/02/2022", box: false, chase: false, exclusive: false, imgUrl: "./img/mattmurdock.jpg"},
-        {id: 4, number: 243, name: "Chell", category: "Pop Games", description: "Issue de la licence Portal, figurine accompagnée du cube iconique du jeux, très bon état, avec boite", purchaseDate: "28/11/2022", box: true, chase: false, exclusive: false, imgUrl: "./img/shell.jpg"}
+        {id: 1, number: 503, name: "Bartman", category: "Pop Television", description: "Offert pas Jacques lors de mon anniversaire, état neuf", publishDate: "2022-9-26 13:53:28", purchaseDate: "20/07/2022", box: true, chase: false, exclusive: true, imgUrl: "./img/bartman.jpg"},
+        {id: 2, number: 421, name: "Eleven with eggos", category: "Pop Television", description: "Chase édition acheté à Geek'n'pop dans un magasin à Tours, état neuf", publishDate: "2022-9-27 13:53:28", purchaseDate: "14/05/2022", box: true, chase: true, exclusive: true, imgUrl: "./img/eleven.jpg"},
+        {id: 3, number: 121, name: "Matt Murdock", category: "Pop Marvel", description: "Récupéré d'occasion dans un micromania, pas très cher mais sans boite", publishDate: "2022-9-25 13:53:28", purchaseDate: "05/02/2022", box: false, chase: false, exclusive: false, imgUrl: "./img/mattmurdock.jpg"},
+        {id: 4, number: 243, name: "Chell", category: "Pop Games", description: "Issue de la licence Portal, figurine accompagnée du cube iconique du jeux, très bon état, avec boite", publishDate: "2022-9-24 13:53:28", purchaseDate: "28/11/2022", box: true, chase: false, exclusive: false, imgUrl: "./img/shell.jpg"}
     ],
         // Reducers (initialState, action)
     reducers: {

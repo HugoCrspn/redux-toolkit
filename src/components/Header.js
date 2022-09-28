ce8e1174ea377d3dfc8c76d9b6707ad3e2330439
HugoCrspn <hugocrespinpro@gmail.com>
2022-09-28 15:07:37 +0200

    scrollTo in Header.js onClick btn (Add Figurine +) #20

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,13 +2,25 @@ import React from 'react';
 
 const Header = () => {
 
+    const handleClick = (event) => {
+        event.preventDefault();
+        const target = document.getElementById('add-figurine-form').offsetTop;
+
+        window.scrollTo({
+            left: 0,
+            top: target - 280
+        })
+
+        setTimeout(function () {document.getElementById("number").focus()}, 600);
+    } 
+
     return (
         <div className='container header-container'>
             <div className="block-text">
                 <h1>Redux-toolkit</h1>
                 <h2>discovering</h2>
                 <p className="baseline">Using redux-toolkit in small web app who manage funko pop figurines collections</p>
-                <button className='btn'>Add figurine</button>
+                <button onClick={handleClick} className='btn'>Add figurine</button>
             </div>
             <img src="./img/illustrations-header.png" alt="Illustrations en 3D" />
         </div>

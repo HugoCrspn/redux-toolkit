1ab5e9813c21bd74372a041792564958a4edde72
HugoCrspn <hugocrespinpro@gmail.com>
2022-09-29 10:01:02 +0200

    Using framer-motion to add simple animations #21

--- src/components/FigurinesList.js
+++ src/components/FigurinesList.js
@@ -9,22 +9,24 @@ const FigurinesList = () => {
     const figurines = useSelector(state => state.figurine);
 
     const figurinesSortByDate = figurines
-    .slice()
-    .sort((a, b) => {
-        if (b.publishDate < a.publishDate) {return -1;}
-        if (b.publishDate > a.publishDate) {return 1;}
-        return 0;
+        .slice()
+        .sort((a, b) => {
+            if (b.publishDate < a.publishDate) { return -1; }
+            if (b.publishDate > a.publishDate) { return 1; }
+            return 0;
     });
 
-    return(
-        <div className='container figurines-container'>
+    return (
+        <div
+            className='container figurines-container'
+        >
             {figurinesSortByDate
-            .map((figurine) => (
-                <FigurineItem 
-                    key={figurine.id}
-                    figurine={figurine}
-                />
-            ))}
+                .map((figurine) => (
+                    <FigurineItem
+                        key={figurine.id}
+                        figurine={figurine}
+                    />
+                ))}
         </div>
     )
 }
